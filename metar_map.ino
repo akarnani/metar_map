d6d322265de7caaf10e80b2c3283c37cbfeb05a8
Andrew Karnani <andrew@akarnani.com>
2023-11-03 00:00:50 -0700

    Update to new aviation weather metar API
    Several other improvements too:
    * ArduinoOTA support
    * Nonblocking timers for better responsiveness to commands

--- metar_map.ino
+++ metar_map.ino
@@ -6,7 +6,9 @@
 #include <string.h>
 
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiMulti.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 #include <ESP8266HTTPClient.h>
 
@@ -17,8 +19,9 @@
 RemoteDebug Debug;
 
 
-#define LOOP_INTERVAL 1500 // ms - interval between brightness updates and lightning strikes
+#define BRIGHTNESS_INTERVAL 1500 // ms - interval between brightness updates
 #define REQUEST_INTERVAL 300000 // How often we update. In practice LOOP_INTERVAL is added. In ms (5 min is 300000)
+#define RETRY_INTERVAL 30000
 
 
 #define BRIGHTNESS 30 // 20-30 recommended. If using a light sensor, this is the initial brightness on boot.
@@ -47,20 +50,20 @@ Adafruit_VEML7700 veml = Adafruit_VEML7700();
 #define ENTRY_DATA 4
 #define ENTRY_SIZE 5
 #define SKIP_HEADER_LINES 6
+#define BUF_SIZE 2048
+#define STATION_INDEX 1
+#define CONDITION_INDEX 30
 
 
-String url = "https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars"
+String url = "https://aviationweather.gov/cgi-bin/data/dataserver.php?dataSource=metars"
              "&requestType=retrieve&format=csv"
              "&hoursBeforeNow=3&mostRecentForEachStation=true"
-             "&fields=station_id,flight_category,cloud_base_ft_agl"
              "&stationString=";
 
 struct entry {
   char name[ENTRY_SIZE];
   char condition[ENTRY_SIZE];
 };
-
-ESP8266WiFiMulti WiFiMulti;
 
 
 #define NUM_AIRPORTS 72 // This is really the number of LEDs
@@ -152,7 +155,8 @@ String airports[NUM_AIRPORTS] = {
 
 
 
-unsigned int loops = 0;
+unsigned long brightnessNext;
+unsigned long requestNext;
 
 
 void setup() {
@@ -175,8 +179,13 @@ void setup() {
 
   WiFi.mode(WIFI_STA);
   WiFi.hostname("metar_map.local");
-  WiFiMulti.addAP(SECRET_SSID, SECRET_PASSWORD);
-  WiFiMulti.run();
+  WiFi.begin(SECRET_SSID, SECRET_PASSWORD);
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
 
   strip.Begin();
   strip.Show();
@@ -210,12 +219,46 @@ void setup() {
     url += airports[i];
   }
 
+  Serial.println(url);
+
   Debug.begin("remotedebug"); // Initialize the WiFi server
   Debug.setResetCmdEnabled(true); // Enable the reset command
   Debug.showColors(true); // Colors
-//  Debug.setSerialEnabled(true);
+  Debug.setSerialEnabled(true);
 
-  debugV("%s", url);
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  
 }
 
 
@@ -233,7 +276,7 @@ void adjustBrightness() {
     brightness = brightness_percent * (MAX_BRIGHTNESS - MIN_BRIGHTNESS) + MIN_BRIGHTNESS;
   }
 
-  debugD("Light reading: %f, %d brightness\n", reading, brightness);
+  debugD("Light reading: %f, %d brightness", reading, brightness);
   strip.SetBrightness(brightness);
 }
 #endif
@@ -242,38 +285,34 @@ void loop() {
   uint8_t oldb = strip.GetBrightness();
 
 #if USE_LIGHT_SENSOR
+  if (millis() >= brightnessNext) {
   adjustBrightness();
+  brightnessNext = millis() + BRIGHTNESS_INTERVAL;
+  }
 #endif
 
   digitalWrite(LED_BUILTIN, LOW);
-  unsigned int threshold = 1;
-  if (USE_LIGHT_SENSOR) threshold = REQUEST_INTERVAL / LOOP_INTERVAL;
-
-  debugV("loop: %d, threshold: %d\n", loops, threshold);
   bool success = false;
 
 
   // if we've waited long enough or its the first run
-  if (strip.GetBrightness() != 0 && (loops >= threshold || loops == 0 || oldb == 0)) {
-    loops = 0;
+  if (strip.GetBrightness() != 0 && (( millis() >= requestNext) || oldb == 0)) {
     debugI("updating metars");
     // wait for WiFi connection
-    if ((WiFiMulti.run() == WL_CONNECTED)) {
+    if ((WiFi.waitForConnectResult() == WL_CONNECTED)) {
       success = getMetars();
+      requestNext = millis() + REQUEST_INTERVAL;
     } else {
-      debugW("[HTTPS] Unable to connect\n");
+      debugW("[HTTPS] Unable to connect");
+      requestNext = millis() + RETRY_INTERVAL;
     }
+
   }
 
   strip.Show();
   Debug.handle();
   digitalWrite(LED_BUILTIN, HIGH);
-  loops++;
-  if (!success || USE_LIGHT_SENSOR) {
-    delay(LOOP_INTERVAL);
-  } else {
-    delay(REQUEST_INTERVAL);
-  }
+  ArduinoOTA.handle();
 
 }
 
@@ -304,7 +343,7 @@ bool getMetars() {
     // httpCode will be negative on error
     if (httpCode > 0) {
       // HTTP header has been send and Server response header has been handled
-      debugD("[HTTPS] GET... code: %d\n", httpCode);
+      debugD("[HTTPS] GET... code: %d", httpCode);
 
       // file found at server
       if (httpCode == HTTP_CODE_OK) {
@@ -313,7 +352,7 @@ bool getMetars() {
         ret = true;
       }
     } else {
-      debugE("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      debugE("[HTTPS] GET... failed, error: %s", https.errorToString(httpCode).c_str());
       strip.ClearTo(white);
     }
 
@@ -326,7 +365,7 @@ bool getMetars() {
 void handleMetars(WiFiClient& input) {
   unsigned int lines = 0;
   unsigned int length = 0;
-  char buf[128];
+  char buf[BUF_SIZE];
   signed char c;
   entry e;
 
@@ -334,13 +373,12 @@ void handleMetars(WiFiClient& input) {
     yield(); //don't get a soft reset due to WDT
 
     if (c == '\n') {
-      buf[length % 128] = '\0';
+      buf[length % BUF_SIZE] = '\0';
       length = 0;
       lines++;
       debugV("%s", buf);
 
       if (lines > SKIP_HEADER_LINES) updateLEDForEntry(processLine(buf));
-
       continue;
     }
 
@@ -349,25 +387,33 @@ void handleMetars(WiFiClient& input) {
       continue;
     }
 
-    buf[length++ % 128] = c;
+    buf[length++ % BUF_SIZE] = c;
 
   }
 }
 
-entry processLine(char input[]) {
+entry processLine(char *input) {
   entry e;
 
-  char* tok = strtok(input, ",");
-  char* prev = tok;
+  int token = 0;
 
-  strlcpy(e.name, tok, ENTRY_SIZE);
-
-  while (tok != NULL) {
-    prev = tok;
-    tok = strtok(NULL, ",");
+  while(1) {
+    char *p = strchr(input, ',');
+    if (p != NULL) {
+      *p = '\0';
+      if (token == STATION_INDEX) {
+        strlcpy(e.name, input, ENTRY_SIZE);
+      } else if (token == CONDITION_INDEX) {
+        strlcpy(e.condition, input, ENTRY_SIZE);
+        break;
+      }
+      token++;
+    } else {
+      break;
+    }
+    input = p + 1;
   }
 
-  strlcpy(e.condition, prev, ENTRY_SIZE);
 
   return e;
 }
@@ -376,7 +422,7 @@ void updateLEDForEntry(entry e) {
 
   for (int i = 0; i < NUM_AIRPORTS; i++) {
     if (strcmp(e.name, airports[i].c_str()) == 0) {
-      debugD("found %s: %s\n", e.name, e.condition);
+      debugD("found %s: %s", e.name, e.condition);
       if (strcmp(e.condition, "VFR") == 0) {
         strip.SetPixelColor(i, green);
       } else if (strcmp(e.condition, "MVFR") == 0) {
@@ -387,11 +433,11 @@ void updateLEDForEntry(entry e) {
         strip.SetPixelColor(i, magenta);
       } else {
         // happens if we don't get a flight category
-        debugD("Got condition %s for airport %s\n", e.condition, e.name);
+        debugD("Got condition %s for airport %s", e.condition, e.name);
       }
       return;
     }
   }
 
-  debugD("Not found: %s\n", e.name);
+  debugD("Not found: %s", e.name);
 }
